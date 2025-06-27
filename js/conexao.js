e6f1336c94b1add58e68d2bcf97400b01e466e08
herickMachado01 <160595828+herickMachado01@users.noreply.github.com>
2025-06-26 21:05:49 -0300

    Update conexao.js

--- js/conexao.js
+++ js/conexao.js
@@ -1,7 +1,1 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = '';
-const supabaseKey = ''; 
-const supabaseClient = createClient(supabaseUrl, supabaseKey);
-
-export default supabaseClient;
